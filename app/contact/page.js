699a920d01a3fc887d54d1e84b7734d7ecc23eb3
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-09-05 19:44:19 +0530

    Update contact/page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,5 @@
-
 "use client";
 import Link from "next/link";
-
 export default function Contact() {
   return (
     <div className="max-w-2xl mx-auto p-8 my-16 bg-orange-100 rounded-lg shadow-lg">
