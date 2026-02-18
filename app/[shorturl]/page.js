b6c4c1b472f47e3c2f41c03dfa7d5c5dfe265340
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-02-18 13:10:26 +0530

    Fixed dynamic short URL page build issue

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,23 +1,5 @@
-/*import { redirect } from "next/navigation"
-import clientPromise from "../../lib/mongodb"
 
-export default async function Page({ params }) {
-  const shorturl = params.shorturl; 
-  const client = await clientPromise;
-  const db = client.db("QuickLink");
-  const collection = db.collection("url");
-
-
-    const doc = await collection.findOne({ shorturl });
-
-    if (doc) {
-
-        redirect(doc.url)
-    }else {
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
-    return <div>My Post: {shorturl}</div>
-}*/
+export const dynamic = "force-dynamic";
 
 import { redirect } from "next/navigation";
 import clientPromise from "../../lib/mongodb";
