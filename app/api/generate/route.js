bc074f744cb30c9cf2e2cf4493678f104cb3d429
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-02-18 13:05:27 +0530

    Fixed pipeline failure due to dynamic route

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,27 +1,29 @@
 import clientPromise from "@/lib/mongodb";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request) {
     
     const body = await request.json();
-    // Validate input: url and shorturl must not be empty
+
     if (!body.url || !body.shorturl || body.url.trim().length === 0 || body.shorturl.trim().length === 0) {
         return Response.json({ success: false, error: true, message: 'URL and Short URL must not be empty' });
     }
+
     const client = await clientPromise;
     const db = client.db("QuickLink");
     const collection = db.collection("url");
 
-    //Check If the short url is exist or not
     const doc = await collection.findOne({ shorturl: body.shorturl });
+
     if (doc) {
         return Response.json({ success: false, error: true, message: 'Short URL already exists' })
     }
 
-    const result = await collection.insertOne({
+    await collection.insertOne({
         url: body.url,
         shorturl: body.shorturl
-    })
+    });
 
-
-  return Response.json({ success: true, error: false, message: 'URL generated successfully' })
+    return Response.json({ success: true, error: false, message: 'URL generated successfully' })
 }
